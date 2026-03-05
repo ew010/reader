6d054dc5094814c9ec525f8a3ffd168ab84f748b
pc <pc@pcdeMacBook-Air.local>
2026-03-05 16:31:58 +0800

    Stabilize widget test to avoid garbled text assertion

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,17 +1,10 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:pdf_reader_flutter/main.dart';
 
 void main() {
   testWidgets('app boots', (WidgetTester tester) async {
     await tester.pumpWidget(const PdfReaderApp());
-    expect(find.text('点击“打开PDF”开始'), findsOneWidget);
+    expect(find.byType(MaterialApp), findsOneWidget);
   });
 }
